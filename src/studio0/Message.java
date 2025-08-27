440d956b81f25486bf251d7f3554abd570bd848d
N1KX-x <xiaonikotianle@gmail.com>
2025-08-27 13:33:40 -0500

    Studio 0 finished

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -3,6 +3,6 @@ package studio0;
 public class Message {
 
 	public static void main(String args[]) {
-		System.out.println(" My name is Niko ");
+		System.out.println(" My name is Niko and I am in Group with Will ");
 	}   
 }
